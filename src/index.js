3bc2e5682d17cbb35bb17467610bfbad012464fc
Amilcar Erazo <ajose.erazo@gmail.com>
2021-01-25 15:08:07 -0500

    .

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const App = () => {
   return <div>Hola</div>;
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
