1cb44daf3e19e0cf1497ce779eb58d878915f79f
Amilcar Erazo <ajose.erazo@gmail.com>
2021-01-25 16:30:38 -0500

    .

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 console.log(ReactDOM);
 
@@ -10,7 +10,10 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root"),
+  () => {
+    console.log("Hola");
+  }
 );
 
 // If you want to start measuring performance in your app, pass a function
